8fc6df6cef8775001d141c78a189ebb08ac424f1
zyich <zuilinho@yandex.ru>
2023-11-01 19:10:30 +0100

    make second part , make and fill start array

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,6 +1,20 @@
 ﻿Console.WriteLine("write number of elements in array :");
 int number = Convert.ToInt32(Console.ReadLine());
 
-
+string [] stringArray = new string[number];
+     for (int i = 0; i < number; i++)
+        {
+            Console.Write($"Input word, number or symbol {i + 1}: ");
+            string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                    {
+                        Console.WriteLine("it is not possible to leave empty string, try again :");
+                        i--; 
+                    }
+                else
+                    {
+                    stringArray[i] = input;
+                    }
+        }
 
 
